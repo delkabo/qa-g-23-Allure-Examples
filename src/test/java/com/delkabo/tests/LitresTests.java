0726797207976319c78486b690cd8e0acf6f06ca
delkabo <delkabo@yandex.ru>
2022-05-05 19:50:48 +0300

    rename LitersTest

--- src/test/java/com/delkabo/tests/LitresTests.java
+++ src/test/java/com/delkabo/tests/LitresTests.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 
-public class GeneratedTests extends TestBase {
+public class LitresTests extends TestBase {
 
 
     @Test
